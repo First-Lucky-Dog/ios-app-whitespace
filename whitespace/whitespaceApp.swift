d6a4ddcc2e5d04d3fad3e053541d85ffd9893656
menglicheng5-eng <menglicheng5@gmail.com>
2026-05-26 11:35:21 +0800

    Initial Commit

---
+++ whitespace/whitespaceApp.swift
@@ -0,0 +1,32 @@
+//
+//  whitespaceApp.swift
+//  whitespace
+//
+//  Created by 白无常 on 2026/5/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct whitespaceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
